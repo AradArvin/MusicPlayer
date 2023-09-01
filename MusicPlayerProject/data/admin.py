cffbec4fb9c0d67db28fad2cc1d47cf8b8195282
arad <aradarvinxo@gmail.com>
2023-09-01 11:26:02 +0330

    chore: models registered to admin

--- MusicPlayerProject/data/admin.py
+++ MusicPlayerProject/data/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import (
+    Artist, 
+    Song, 
+    Playlist, 
+    Comment, 
+    Genre, 
+    Like,
+    )
 # Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(Artist)
+admin.site.register(Song)
+admin.site.register(Playlist)
+admin.site.register(Like)
+admin.site.register(Comment)
+
