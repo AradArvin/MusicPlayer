1979f27e47ae354d6370b7474ba2fb2269f43aaa
arad <aradarvinxo@gmail.com>
2023-09-02 17:38:32 +0330

    chore: forms for signup and login created

---
+++ MusicPlayerProject/accounts/forms.py
@@ -0,0 +1,24 @@
+from django.forms import ModelForm
+from django import forms
+from .models import CustomUser
+
+
+class SignUpForm(ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ["username", "password"]
+
+    def clean_data(self):
+        cleaned_data = super(CustomUser, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Invalid password! Please try again")
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
