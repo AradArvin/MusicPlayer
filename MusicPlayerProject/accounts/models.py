10a9f9448ea834b992767f6498916a5ddbdcaf47
arad <aradarvinxo@gmail.com>
2023-09-02 14:22:17 +0330

    chore: custom user model created

--- MusicPlayerProject/accounts/models.py
+++ MusicPlayerProject/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 # Create your models here.
+
+
+class CustomUser(AbstractUser, PermissionsMixin):
+    class AccountType(models.TextChoices):
+        VIP = "V", _("VIP")
+        FREE = "F", _("Free")
+
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    type = models.CharField(choices=AccountType.choices, max_length=1)
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    profile_pic = models.ImageField(upload_to="user", blank=True, null=True)
+    date_added = models.DateTimeField(default=timezone.now, editable=False, blank=True)
+
+    def __str__(self) -> str:
+        return self.username
