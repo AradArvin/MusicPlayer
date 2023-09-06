3744b24750f2f4a5ea3ac7a4b98cfb152207ed93
arad <aradarvinxo@gmail.com>
2023-09-06 23:18:37 +0330

    chore: cusom user registered to admin page

--- MusicPlayerProject/accounts/admin.py
+++ MusicPlayerProject/accounts/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import CustomUser
 # Register your models here.
 
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserDisplay(admin.ModelAdmin):
+    list_display = ('username', 'email', 'acc_type', 'date_joined')
+    list_filter = ('acc_type',)
+    search_fields = ('username',)
+    list_per_page = 15
+    
