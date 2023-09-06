012bb1ee8eba164e0aba157212e3c2dba04913bb
arad <aradarvinxo@gmail.com>
2023-09-06 23:30:51 +0330

    chore: urls for signup and logout added

--- MusicPlayerProject/accounts/urls.py
+++ MusicPlayerProject/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import LoginView
+from .views import *
 
 urlpatterns =[
-    path("login/", LoginView.as_view(), name="login")
+    path("login/", LoginView.as_view(), name="login"),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
