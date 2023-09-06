e9cf5a8f0ced5119b74b6ac09c88294166d9be04
arad <aradarvinxo@gmail.com>
2023-09-06 23:36:58 +0330

    chore: urls for song list and song detail

--- MusicPlayerProject/data/urls.py
+++ MusicPlayerProject/data/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from .views import *
 
-
-urlpatterns =[]
+urlpatterns =[
+    path("", HomeView.as_view(), name="home"),
+    path("songs/", SongListView.as_view(), name="songlist"),
+    path("song/<slug:slug>/", SongDetailView.as_view(), name="songlist"),
+]
