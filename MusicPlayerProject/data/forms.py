906f8c9a9dfa66714650554548ed7d6e3df09cf9
arad <aradarvinxo@gmail.com>
2023-09-06 23:35:25 +0330

    chore: form for csv import

--- MusicPlayerProject/data/forms.py
+++ MusicPlayerProject/data/forms.py
@@ -1,4 +1,16 @@
+from django.db import models
+from django.forms import forms
+from django.forms import ModelForm
+from .models import Comment
+from django import forms
+
+
 class AddComment(ModelForm):
     class Meta:
         model = Comment
         fields = ['commentz']
+
+
+
+class CsvImportForm(forms.Form):
+    csv_file = forms.FileField()
