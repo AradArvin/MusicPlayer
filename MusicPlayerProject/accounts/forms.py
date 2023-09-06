0f1cd003d4206a631d98f7c626f43497b33ef890
arad <aradarvinxo@gmail.com>
2023-09-06 23:29:49 +0330

    fix: method name and super func fixed

--- MusicPlayerProject/accounts/forms.py
+++ MusicPlayerProject/accounts/forms.py
@@ -11,14 +11,17 @@ class SignUpForm(ModelForm):
         model = CustomUser
         fields = ["username", "email", "password"]
 
-    def clean_data(self):
-        cleaned_data = super(CustomUser, self).clean()
+    def clean(self):
+        cleaned_data = super(SignUpForm, self).clean()
         password = cleaned_data.get("password")
         confirm_password = cleaned_data.get("confirm_password")
 
         if password != confirm_password:
-            raise forms.ValidationError("Invalid password! Please try again")
+            raise forms.ValidationError(
+                "Password confirmation failed!"
+            )
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+    
