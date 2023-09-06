164840a2a97789f7a00b45520e25a12f3aa79c9c
arad <aradarvinxo@gmail.com>
2023-09-06 23:33:41 +0330

    chore: admin site header and title changed

--- MusicPlayerProject/core/admin.py
+++ MusicPlayerProject/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+
+admin.site.site_header = 'Music World Admin'
+admin.site.site_title = 'Music World Admin'
+admin.site.index_title = 'Welcome to Music World Administration'
